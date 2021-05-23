22a21f0b6899b2710b1c52f700cea8c3ce6d7eac
unknown <mohdyawars@gmail.com>
2021-05-23 20:47:22 +0530

    result.dart updated

--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 30) {
+      resultText = 'You are awesome and innocent';
+    } else if (resultScore <= 60) {
+      resultText = 'Pretty Likeable';
+    } else if (resultScore <= 90) {
+      resultText = 'You are ... Strange';
+    } else {
+      resultText = 'You are so bad!';
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it'),
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            onPressed: resetHandler,
+            child: Text('Restart Quiz!'),
+            textColor: Colors.blue,
+          ),
+           
+        ],
+      ),
     );
   }
 }
