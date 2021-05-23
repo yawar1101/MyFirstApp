8ab0c3835c9410a443431badc318995558a6c386
unknown <mohdyawars@gmail.com>
2021-05-23 15:34:20 +0530

    Intial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/result.dart';
 
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
+
 //void main() {
 //  runApp(MyApp());
 //}
@@ -17,9 +19,32 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Black', 'Red', 'Green', 'White'],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+    },
+    {
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': ['Yawar', 'Yawar', 'Yawar', 'Yawar'],
+    },
+    {
+      'questionText': 'What\'s your favorite movie?',
+      'answers': ['Titanic', 'Interstellar', 'Inception', 'Batman Begins'],
+    },
+  ];
   var _questionIndex = 0;
 
   void _answerQuestion() {
+    if (_questionIndex < _questions.length) {
+      print("We have more questions!");
+    } else {
+      print('No more questions!');
+    }
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -28,46 +53,22 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['Black', 'Red', 'Green', 'White'],
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
-      },
-      {
-        'questionText': 'Who\'s your favorite instructor?',
-        'answers': ['Yawar', 'Yawar', 'Yawar', 'Yawar'],
-      },
-      {
-        'questionText': 'What\'s your favorite movie?',
-        'answers': ['Titanic', 'Interstellar', 'Inception', 'Batman Begins'],
-      },
-    ];
-
-   // var dummy = ['Hello'];
-   // dummy.add('Yawar');
-   // print(dummy);
-
+    // var dummy = ['Hello'];
+    // dummy.add('Yawar');
+    // print(dummy);
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIndex]['questionText'],
-            ),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(),
       ),
     );
   }
